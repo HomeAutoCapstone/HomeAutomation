33d7235e5ef3b113c0bb69c4cb6fed35d9aa007a
atwoodwang0918 <atwoodwang@gmail.com>
2015-04-07 20:49:40 -0400

    temp_refined

--- web2py/applications/HomeAutomation/controllers/temp.py
+++ web2py/applications/HomeAutomation/controllers/temp.py
@@ -1,18 +1,21 @@
 from datetime import *
+import temp_list
 
-temp={'Tue':{(19,58):27}}
 #when it's Tue 19:58, change temp to 27 
-def tem(temp):
-	now=datetime.now()
-	day=now.strftime( '%a' )
-	hour=now.hour
-	minute=now.minute
-	if temp.has_key(day):
-		get=temp[day]
-		if get.has_key((hour,minute)):
-			t=get[(hour,minute)]
+class temperature(object):
+	def __init__(self):
+		self.temp=temp_list.temp
+	def tem(self):
+		now=datetime.now()
+		day=now.strftime( '%a' )
+		hour=now.hour
+		minute=now.minute
+		if self.temp.has_key(day):
+			get=self.temp[day]
+			if get.has_key((hour,minute)):
+				t=get[(hour,minute)]
+			else:
+				t='stay'
 		else:
 			t='stay'
-	else:
-		t='stay'
-	return t
+		return t
---
+++ web2py/applications/HomeAutomation/controllers/test_temp.py
@@ -0,0 +1,4 @@
+import temp
+a=tem.temperature()
+b=a.tem()
+print b
