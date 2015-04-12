1712934b6c69301e863b36aa2babcb7df184373b
atwoodwang0918 <atwoodwang@gmail.com>
2015-04-12 17:27:27 -0400

    a

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 password="pbkdf2(1000,20,sha512)$a91cc406bf52ea7c$016a15dba298fc7e8f7cb06a637449e26175f1a6"
+=======
+password="pbkdf2(1000,20,sha512)$9e5d9f9f21b41625$7fcbb34277e69fdd02998d0f3c3737d1fc8d2988"
+>>>>>>> Stashed changes
