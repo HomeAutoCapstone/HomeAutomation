ecb4708b5b68fbb09cafc4bc162238855bafa014
atwoodwang0918 <atwoodwang@gmail.com>
2015-04-16 16:16:22 -0400

    made frontpage prettier and made the room setting page

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,5 +1,1 @@
-<<<<<<< Updated upstream
-password="pbkdf2(1000,20,sha512)$b9577be0df027e2a$e23d769ca5f1276bddc4ae46c952ead9c4da96fb"
-=======
-password="pbkdf2(1000,20,sha512)$8f0b967277bf017e$14fc0f7b66f3e19ec1ba368ac3d853f3b4d9d39b"
->>>>>>> Stashed changes
+password="pbkdf2(1000,20,sha512)$b8592abc1246f177$b6f3923f084b3047c8725be535073dc7f2e51903"
